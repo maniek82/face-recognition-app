9cac95963930fa40dcb41eaa3a407efff88e9795
maniek82 <mariusz.tripler@gmail.com>
2018-09-14 16:58:00 +0100

    change env in server

--- server.js
+++ server.js
@@ -51,8 +51,8 @@ app.post('/imageurl',(req,res)=>{image.handleApiCall(req,res)})
 
 
 
-app.listen(process.env.PORT,()=> {
-    console.log(`Server running on port ${process.env.PORT}`)
+app.listen(3000,()=> {
+    console.log(`Server running on port 3000`)
 })
 
 console.log("console.log ", process.env.PORT);
